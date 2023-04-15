449ca57f4f49edc74a0a04ad2e3aab89657ed5d5
Kayla Copeland <kaylabebusy@gmail.com>
2023-04-15 19:54:28 -0400

    lab redone

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+const name = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(name, eventNames) {
+    let messages = [];
+    for(let i = 0; i < name.length; i++) {
+    messages.push(`Thank you, ${name[i]}, for the wonderful ${eventNames} gift!`)
+    }
+    return messages;
+  }
+  
+  function countDown(number){
+    while(number >= 0){
+      console.log(number);
+      number --
+    }
+  }
+
+  console.log["Guadalupe,", "Ollie,", "Aki,"];
+  console.log(writeCards(names,));
+  
